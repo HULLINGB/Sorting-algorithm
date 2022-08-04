c0e6dc3a2f4672abfc3bd191018ed2af1a633d08
HULLINGB <bradley.hullinger@ucdenver.edu>
2022-08-03 23:56:04 -0600

    Update SortingAlgorithm.py

--- SortingAlgorithm.py
+++ SortingAlgorithm.py
@@ -3,8 +3,9 @@ array = [3, 1, 5, 1, 6, 9, 7]
 placeholder = 0
 
 for x in range(7):
-	for y in range(7);
+	for y in range(7):
 		placeholder = array[x]
         array[x] = array[y]
         array[y] = placeholder
+
 print(array)
