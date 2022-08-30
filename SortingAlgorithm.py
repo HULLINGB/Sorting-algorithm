7c5e33c9a318865c75078f298278fc276b143253
HULLINGB <bradley.hullinger@ucdenver.edu>
2022-08-30 08:49:36 -0600

    Update error in SortingAlgorithm.py

--- SortingAlgorithm.py
+++ SortingAlgorithm.py
@@ -1,11 +1,12 @@
-array = [3, 1, 5, 1, 6, 9, 7]
+array = [9, 1, 4, 1, 3, 0, 5]
 
 placeholder = 0
 
 for x in range(7):
-	for y in range(7):
-		placeholder = array[x]
-        array[x] = array[y]
-        array[y] = placeholder
+    for y in range(7):
+        if array[x] < array[y]:
+            placeholder = array[x]
+            array[x] = array[y]
+            array[y] = placeholder
 
 print(array)
