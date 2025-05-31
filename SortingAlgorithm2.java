1b71dac1483a203ef8250ee2dcde73f219bcdd75
Brad Hullinger <bradley.hullinger@ucdenver.edu>
2025-05-30 22:40:38 -0700

    Update SortingAlgorithm2.java

--- SortingAlgorithm2.java
+++ SortingAlgorithm2.java
@@ -140,26 +140,175 @@ public class SortingAlgorithm2DArray{
                                                }
                                               for(int a = 0; a < array2.length; a++)
                                               {
-                                               str = “”;
+                                               import java.util.*;
+
+public class SortingAlgorithm2DArray{
+
+     public static void main(String []args){
+                                   Arraylist<String> list = new Arraylist<>();
+                                   int dimensionLength = 2;
+                                   int numOfDimensions = 2;
+			int[][] arrayMain = new int[dimensionLength][dimensionLength];
+                                   int[] array = new int[dimensionLength * numOfDimensions];
+                                  int[] array2 = new int[dimensionLength * numOfDimensions];
+                                  int[] arrayTrack =  new int[dimensionLength * numOfDimensions];
+                                  int[] arrayHolder =  new int[dimensionLength * numOfDimensions];
+			int number = 0;
+			int space = 0;
+			int length = array.length;
+			if (array2.getClass().getComponentType().isArray()) {
+				System.out.println("This is a multidimensional array");
+			}else{
+				System.out.println("This is not a multidimensional array");
+			}
+			    Random rand = new Random();
+				for(int a = 0; a < array2.length; a++)
+			    {
+			        for(int b = 0; b < array[0].length; b++)
+			        {
+						for(int c = 0; c < array[0].length; c++)
+						{
+							number = rand.nextInt(10);
+							arrayMain[a][b][c] = number;
+						}
+			        }
+			    }
+                                       int i = 0;
+                                        for(int a = 0; a < dimensionLength; a++)
+                                        {
+                                          for(int b = 0; b < dimensionLength; b++)
+                                          }
+                                               list[i] = arrayMain[a][b].toString();
+                                               i++;
+                                               }
+}
+                                               i = 0;
+                                               for(int a = i; a < list.size(); a++)
+                                              {
+                                              i = x;
+                                               char[] char = list.get(a).toCharArray();
+                                               for(int b = 0; b < list.size(); b++)
+                                               {
+                                                  if(char[b] == ‘,’)
+                                                 {
+                                                       array[i] = str;
+                                                       str = “”;
+                                                       i++;
+                                                       for(int c = b; c < list.size(); c++)
+                                                      {
+                                                          if(char[c] == ‘,’)
+                                                          {
+                                                          i = 0;
+                                                          array2[i] = str;
+                                                          str = “”;
+                                                                for(int d = c; d < list.size(); d++)
+                                                               {
+                                                          if(char[d] == ‘,’)
+                                                          {
+                                                          i = 0;
+                                                          array3[i] = str;
+                                                          str = “”;
+                                                         }
+                                                          str = str + char[d];
+                                                         }
+                                                         }
+                                                          str = str + char[c];
+                                                         }
+                                                    }
+                                               str = str + char[b];
+                                               }
+                                             x++;
+                                            }
+                                            i = 0;
+				for(int a = 0; a < array.length; a++)
+				{
+					for(int b = 0; b < array.length; b++)
+					{
+						if(array[a] < array[b])
+						{
+						space = array[a];
+						array[a] = array[b];
+						array[b] = space;
+                                                                      arrayTrack[i] = b;
+                                                                      i++;
+						}
+					}
+				}
+                                               for(int a = 0; a < array2.length; a++)
+                                              {
+                                                    arrayHolder[a] = array2[arrayTracker[a]];
+                                                    arrayHolder2[a] = array3[arrayTracker[a]];
+                                               }
+                                              for(int a = 0; a < array2.length; a++)
+                                              {
+                                                    array2[a] = arrayHolder[a];
+                                               }
+                                               for(int a = 0; a < array2.length; a++)
+                                              {
+                                                    arrayHolder[a] = 0;
+                                                    arrayTracker[a] = 0;
+                                               }
+                                               i = 0;
+                                               for(int a = 0; a < array2.length; a++)
+				{
+					for(int b = 0; b < array2.length; b++)
+					{
+						if(array2[a] < array2[b])
+						{
+						space = array2[a];
+						array2[a] = array2[b];
+						array2[b] = space;
+                                                                      arrayTrack[i] = b;
+                                                                      i++;
+						}
+					}
+				}	
+                                              for(int a = 0; a < array2.length; a++)
+                                              {
+                                                    arrayHolder2[a] = array3[arrayTracker[a]];
+                                               }
+                                              for(int a = 0; a < array2.length; a++)
+                                              {
+                                                    array2[a] = arrayHolder2[a];
+                                               }
+                                               for(int a = 0; a < array2.length; a++)
+                                              {
+                                                    arrayHolder2[a] = 0;
+                                                    arrayTracker[a] = 0;
+                                               }	
+                                              for(int a = 0; a < array2.length; a++)
+                                              {
+                                                    arrayHolder2[a] = array3[arrayTracker[a]];
+                                               }
+                                              for(int a = 0; a < array2.length; a++)
+                                              {
+                                               i = 0;
+                                               for(int a = 0; a < array4.length; a++)
+                                               {
+                                                        array4[i] = array[a];
+                                                        i++;
+                                                        array4[i] = array2[a];
+                                                        i++;
+                                               }
+                                               i = 0;
                                                for(int a = 0; a < dimensionLength; a++)
                                         {
                                           for(int b = 0; b < dimensionLength; b++)
                                           {
-                                               str = array[i] + “, “ + array2[i];
-                                               arrayMain[a][b] = str;
+                                                                                        arrayMain[a][b] = array4[i];
+                                               i++;
                                                }
                                                }
-}
 //Print
 for(int a = 0; a < dimensionLength; a++)
                                         {
                                           for(int b = 0; b < dimensionLength; b++)
                                           {                                         
-                                               System.out.print(arrayMain[a][b]);
+                                               System.out.print(arrayMain[a][b] + “, “);
                                                }
 System.out.println(“”);
-}
 /**
+}
             System.out.println("{{");
             for(int a = 0; a < arrayMain.length; a++)
             {
@@ -189,7 +338,9 @@ System.out.println(“”);
 				}
             }
             System.out.println("}");
-**/
+   
      }
+**/
 }
+
 
